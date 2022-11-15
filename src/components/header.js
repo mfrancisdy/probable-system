a2a9bb88d934c93055f5423b863e8b6dad93bccd
Francis Sachin <francissachu@gmail.com>
2022-11-15 18:30:52 +0530

    sticky logo

--- src/components/header.js
+++ src/components/header.js
@@ -17,8 +17,10 @@ export default function Header() {
     function scrollFunction() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
             document.getElementById("header").classList.add("header-sticky");
+            document.getElementById('logo').classList.add('logo-sticky');
         } else {
             document.getElementById("header").classList.remove("header-sticky");
+            document.getElementById('logo').classList.remove('logo-sticky');
         }
     }
 
@@ -196,7 +198,7 @@ export default function Header() {
             <Navbar expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img src="/img/logo.png" className="d-inline-block align-top" alt="logo" />
+                        <img id='logo' src="/img/logo.png" className="d-inline-block align-top" alt="logo" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
